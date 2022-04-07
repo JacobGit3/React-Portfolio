b6ad5c2adf95759edd975fe80f045b0622de7f8e
Jacob <jacob.tobin.liberty@gmail.com>
2022-04-07 19:23:54 -0400

    push

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import nav from './components/nav'
-import about from './components/about'
-import contact from './components/contact'
-import projects from './components/projects'
+import React, { useState } from 'react';
+import Nav from './components/nav'
+import About from './components/about'
+import Contact from './components/contact'
+import Projects from './components/projects'
 
 function App() {
+  const [ contactSelected, setContactSelected ] = useState(false);
+
   return (
     <div className="App">
-      
+      <Nav
+        contactSelected = {contactSelected}
+        setContactSelected = {setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Projects></Projects>
+            <About></About>
+          </>
+        ) : (
+          <Contact></Contact>
+        )}
+      </main>
     </div>
   );
 }
