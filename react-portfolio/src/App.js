6360ddc4675c8b315b3d2208f816a2dcbaa67074
Jacob <jacob.tobin.liberty@gmail.com>
2022-04-07 16:38:02 -0400

    inital files

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import nav from './components/nav'
+import about from './components/about'
+import contact from './components/contact'
+import projects from './components/projects'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
