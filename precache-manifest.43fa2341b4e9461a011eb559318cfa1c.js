e6067ae5e2c2491193ffc4b26563493ab07e32f9
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2022-04-17 21:42:20 -0400

    Updates

---
+++ precache-manifest.43fa2341b4e9461a011eb559318cfa1c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "20ca6901a70dd9e792b2",
+    "url": "/React-Portfolio/static/css/main.8baaa163.chunk.css"
+  },
+  {
+    "revision": "20ca6901a70dd9e792b2",
+    "url": "/React-Portfolio/static/js/main.20ca6901.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/React-Portfolio/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "8803a05143572a97c63c",
+    "url": "/React-Portfolio/static/js/2.8803a051.chunk.js"
+  },
+  {
+    "revision": "2416c03ca7e5e810fbf2",
+    "url": "/React-Portfolio/static/js/runtime~main.2416c03c.js"
+  },
+  {
+    "revision": "c56d06565066f8cf73eb36980f7f89f8",
+    "url": "/React-Portfolio/static/media/me.c56d0656.jpg"
+  },
+  {
+    "revision": "4ae3cab7acb2f53ba1f3446f96eb81b6",
+    "url": "/React-Portfolio/static/media/resume.4ae3cab7.png"
+  },
+  {
+    "revision": "2b568ddd13db2ebf1d4877997d2d36a7",
+    "url": "/React-Portfolio/static/media/background.2b568ddd.png"
+  },
+  {
+    "revision": "f40c9da36464d0755a21982a2315f7c2",
+    "url": "/React-Portfolio/static/media/budget.f40c9da3.png"
+  },
+  {
+    "revision": "ae2d57d635aea13742c784ba53ef88da",
+    "url": "/React-Portfolio/static/media/devden.ae2d57d6.png"
+  },
+  {
+    "revision": "7e1826b9f032b913c9fb7c160f694d0f",
+    "url": "/React-Portfolio/static/media/employee.7e1826b9.png"
+  },
+  {
+    "revision": "76b46fe38d4daf0df35696b2b23513d1",
+    "url": "/React-Portfolio/static/media/portfolio.76b46fe3.png"
+  },
+  {
+    "revision": "f0db970702b8d5cdceec1ef8737eb3d6",
+    "url": "/React-Portfolio/static/media/social.f0db9707.png"
+  },
+  {
+    "revision": "7a008edc51b067acd67acf340a55b452",
+    "url": "/React-Portfolio/static/media/socialdb.7a008edc.png"
+  },
+  {
+    "revision": "483778f86ea487a63d50378949a114b2",
+    "url": "/React-Portfolio/static/media/trail.483778f8.png"
+  },
+  {
+    "revision": "de83f21abea3366f59ff793b39fc578b",
+    "url": "/React-Portfolio/static/media/weather.de83f21a.png"
+  },
+  {
+    "revision": "a5eb12fb6fb0782138455ac752d19c2f",
+    "url": "/React-Portfolio/static/media/instagram.a5eb12fb.png"
+  },
+  {
+    "revision": "bcc334855d6d116330a740a7c0d906d6",
+    "url": "/React-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.9f6315123ab09480058dec9270d914be.js"
+  "/React-Portfolio/precache-manifest.43fa2341b4e9461a011eb559318cfa1c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/React-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
